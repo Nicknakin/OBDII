bb5ab268b9f0f72e7e06974fc74ef407efb7a697
chancehiblinds <chance.huddleston@mavs.uta.edu>
2022-11-27 16:40:32 -0600

    WORKING log.txt

--- OBDIIInterface/OBD.py
+++ OBDIIInterface/OBD.py
@@ -133,7 +133,7 @@ def exfiltrate_data(data):
     """
     try:
         #output_file = os.path.join(log_folder,exported_data_file)
-        f = open(exported_data_file, "a+",encoding="utf-8")
+        f = open(exported_data_file, "w+",encoding="utf-8")
         f.write(json.dumps(data,separators=(':',','))+"\n\n")
         f.flush()
         f.close()
